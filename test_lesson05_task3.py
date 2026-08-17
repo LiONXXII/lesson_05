784270cec5d6b292b0b484ea161922e8a3e94415
LiONXXII <nadya03081997@mail.ru>
2026-08-17 19:16:13 +0300

    Первый коммит

---
+++ test_lesson05_task3.py
@@ -0,0 +1,13 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+
+
+def test_links_count():
+    driver = webdriver.Chrome()
+    driver.get("https://httpbin.qa-territory.online/links/10")
+    links = driver.find_elements(By.TAG_NAME, "a")
+    assert len(links) == 9
+    for link in links:
+        assert link.is_displayed()
+    assert "1" in links[0].text
+    driver.quit()
